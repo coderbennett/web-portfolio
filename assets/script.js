9a5d3a7c79bb3400e245e6d04c5a5a05d9c171f5
joey bennett <joeykb17@gmail.com>
2022-04-16 11:44:56 -0700

    add content to about

--- assets/script.js
+++ assets/script.js
@@ -17,11 +17,11 @@ themeBtn.on("click", changeTheme);
 function changeTheme() {
     if (htmlEl.attr('data-theme') === "forest") {
         htmlEl.attr('data-theme', "garden");
-        themeBtnImg.attr('src', "./assets/forestMode.png");
+        themeBtnImg.attr('src', "./assets/gardenMode.png");
         console.log('button pressed, changed to garden theme')
     } else {
         htmlEl.attr('data-theme', "forest");
-        themeBtnImg.attr('src', "./assets/gardenMode.png");
+        themeBtnImg.attr('src', "./assets/forestMode.png");
         console.log('button pressed, changed to forest theme')
     }
     theme = htmlEl.attr('data-theme');
