d4fc8c4a9e79eb85eaab3d0d6534e48a439c1828
joey bennett <joeykb17@gmail.com>
2022-04-16 13:42:39 -0700

    add footer with theme changing capabilities

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var themeBtn = $('#themeBtn');
-var themeBtnImg = $('#themeBtnImg');
+var themeLeafIcons = $('.leafIcon');
 var htmlEl = $('#htmlTag');
+var twitterIconEl = $('#twitterIcon');
+var githubIconEl = $('#githubIcon');
+var linkedinIconEl = $('#linkedinIcon');
 
 var theme = htmlEl.attr('data-theme');
 
@@ -9,25 +12,25 @@ var projects = [ {
     gitHubUrl: "https://github.com/coderbennett/pethub",
     deployedUrl: "https://coderbennett.github.io/pethub/",
     description: "Pethub is a web app for anyone interested in responsibly adopting a pet. On our webapp you can find pets in all areas to adopt.",
-    img: ""
+    media: ""
 }, {
     name: "Weather Dashboard",
     gitHubUrl: "https://github.com/coderbennett/weather-dashboard",
     deployedUrl: "https://coderbennett.github.io/weather-dashboard/",
     description: "The Weather Dashboard is a simple web app which fetchs data from the Open Weather API and presents the user with today's weather forecast plus the next 5 following days.",
-    img: ""
+    media: ""
 }, {
     name: "JavaScript Fundamentals Quiz",
     gitHubUrl: "https://github.com/coderbennett/quiz-javascript-fundamentals",
     deployedUrl: "https://coderbennett.github.io/quiz-javascript-fundamentals/",
     description: "A 10 question JavaScript quiz on some fundamental methods, definitions and syntax.",
-    img: ""
+    media: ""
 }, {
     name: "Word Guess",
     gitHubUrl: "https://github.com/coderbennett/word-guess",
     deployedUrl: "https://coderbennett.github.io/word-guess/",
     description: "This game gives you a random word and it is up to you to type the word before the time runs out!",
-    img: ""
+    media: ""
 }
 ];
 
@@ -37,18 +40,27 @@ if(localStorage.getItem("theme") === null) {
 
 theme = localStorage.getItem("theme");
 htmlEl.attr('data-theme', theme);
-themeBtnImg.attr('src', "./assets/" + theme + "Mode.png");
+themeLeafIcons.attr('src', "./assets/" + theme + "/leafIcon.png");
+twitterIconEl.attr('src', "./assets/" + theme + "/twitterIcon.png");
+githubIconEl.attr('src', "./assets/" + theme + "/githubIcon.png");
+linkedinIconEl.attr('src', "./assets/" + theme + "/linkedinIcon.png");
 
 themeBtn.on("click", changeTheme);
 
 function changeTheme() {
     if (htmlEl.attr('data-theme') === "forest") {
         htmlEl.attr('data-theme', "garden");
-        themeBtnImg.attr('src', "./assets/gardenMode.png");
+        themeLeafIcons.attr('src', "./assets/garden/leafIcon.png");
+        twitterIconEl.attr('src', "./assets/garden/twitterIcon.png");
+        githubIconEl.attr('src', "./assets/garden/githubIcon.png");
+        linkedinIconEl.attr('src', "./assets/garden/linkedinIcon.png");
         console.log('button pressed, changed to garden theme')
     } else {
         htmlEl.attr('data-theme', "forest");
-        themeBtnImg.attr('src', "./assets/forestMode.png");
+        themeLeafIcons.attr('src', "./assets/forest/leafIcon.png");
+        twitterIconEl.attr('src', "./assets/forest/twitterIcon.png");
+        githubIconEl.attr('src', "./assets/forest/githubIcon.png");
+        linkedinIconEl.attr('src', "./assets/forest/linkedinIcon.png");
         console.log('button pressed, changed to forest theme')
     }
     theme = htmlEl.attr('data-theme');
